6a70201d00cc2fbcd5c3171a758966560b1c56c0
tinashelvurayai-dot <tinashelvurayai@gmail.com>
2026-06-04 02:25:48 +0200

    Delete src directory

--- src/types/react-katex.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "react-katex" {
-  import type { ComponentType } from "react";
-  export const InlineMath: ComponentType<{ math: string }>;
-  export const BlockMath: ComponentType<{ math: string }>;
-}
